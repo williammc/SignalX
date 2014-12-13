b23ab9f16c4d7b3ddeb27ba3af4338d93431376a
William McCohen <william.mccohen@gmail.com>
2014-12-13 19:02:16 +0100

    remove function code. update test.

--- test/test_singals.cc
+++ test/test_singals.cc
@@ -3,70 +3,71 @@
 #include <iostream>
 #include <vector>
 
-struct Foo /*: public sigx::Observer*/ {
-    bool handler_a(const char* e) const {
-        std::cout << e << std::endl;
-        return true;
-    }
+/// deriving Observer for automatic disconnection management
+struct Sample /*: public sigx::Observer*/ {
+  bool slot1(const char* e) const {
+    std::cout << e << std::endl;
+    return true;
+  }
 
-    bool handler_b(const char* e, std::size_t n) {
-        std::cout << e << " [on line: " << n << "]" << std::endl;
-        return true;
-    }
+  bool slot2(const char* e, std::size_t n) {
+    std::cout << e << " [on line: " << n << "]" << std::endl;
+    return true;
+  }
 
-    static bool handler_c(const char* e) {
-        std::cout << e << std::endl;
-        return true;
-    }
+  static bool slot3(const char* e) {
+    std::cout << e << std::endl;
+    return true;
+  }
 };
 
-bool handler_d(const char* e, std::size_t n) {
-    std::cout << e << " [on line: " << n << "]" << std::endl;
-    return false;
+bool slot4(const char* e, std::size_t n) {
+  std::cout << e << " [on line: " << n << "]" << std::endl;
+  return false;
 }
 
 int main() {
-    Foo foo;
+  Sample sample;
 
-    // Declare sigx::Signals using function signature syntax
-    sigx::Signal<bool(const char*)> signal1;
-    sigx::Signal<bool(const char*, std::size_t)> signal2;
+  // Declare sigx::Signals using function signature syntax
+  sigx::Signal<bool(const char*)> signal1;
+  sigx::Signal<bool(const char*, std::size_t)> signal2;
 
-    // Connect member functions to sigx::Signals
-    signal1.connect<Foo, &Foo::handler_a>(&foo);
-    signal2.connect<Foo, &Foo::handler_b>(&foo);
+  // Connect member functions to sigx::Signals
+  signal1.connect<Sample, &Sample::slot1>(&sample);
+  signal2.connect<Sample, &Sample::slot2>(&sample);
 
-    // Connect a static member function
-    signal1.connect<Foo::handler_c>();
+  // Connect a static member function
+  signal1.connect<Sample::slot3>();
 
-    // Connect a free function
-    signal2.connect<handler_d>();
+  // Connect a free function
+  signal2.connect<slot4>();
 
-    // Emit Signals
-    signal1("signal 1");
-    signal2("signal 2", __LINE__);
+  // Emit Signals
+  signal1("signal 1");
+  signal2("signal 2", __LINE__);
 
-    std::vector<bool> status;
+  std::vector<bool> status;
 
-    // Emit Signals and accumulate SRVs (signal return values)
-    signal1("signal 1 again", [&](bool srv) {
-        status.push_back(srv);
-    });
+  // Emit Signals and accumulate SRVs (signal return values)
+  signal1("signal 1 again", [&](bool srv) {
+    status.push_back(srv);
+  });
 
-    // Disconnect member functions from a sigx::Signal
-    signal1.disconnect<Foo, &Foo::handler_a>(foo);
-    signal2.disconnect<Foo, &Foo::handler_b>(foo);
+  // Disconnect member functions from a sigx::Signal
+  signal1.disconnect<Sample, &Sample::slot1>(sample);
+  signal2.disconnect<Sample, &Sample::slot2>(sample);
 
-    // Disconnect a static member function
-    signal1.disconnect<Foo::handler_c>();
+  // Disconnect a static member function
+  signal1.disconnect<Sample::slot3>();
 
-    // Disconnect a free function
-    signal2.disconnect<handler_d>();
+  // Disconnect a free function
+  signal2.disconnect<slot4>();
 
-    // Emit again to test disconnects
-    signal1("THIS SHOULD NOT APPEAR");
-    signal2("THIS SHOULD NOT APPEAR", __LINE__);
+  // Emit again to test disconnects
+  signal1("THIS SHOULD NOT APPEAR");
+  signal2("THIS SHOULD NOT APPEAR", __LINE__);
 
-    // Pause the screen
-    std::cin.get();
+  // Pause the screen
+  std::cin.get();
 }
